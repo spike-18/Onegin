89e99945abe76ffd0a7b4ffad4f5e347b0279143
Kirill Madorskii <madorskii.kv@phystech.edu>
2023-09-12 16:59:45 +0300

    getter and setter separated

--- lib.cpp
+++ lib.cpp
@@ -1,23 +1,35 @@
 #include "lib.hpp"
 
 
-void print_separately (int data[], size_t sizeX, size_t sizeY)
+void print_separately (const int data[], size_t sizeX, size_t sizeY)
 {
     printf("\n");
     for(size_t i = 0; i < sizeX; i++)
     {
         for(size_t j = 0; j < sizeY; j++)
-            printf("%02d ", get_set('g', data, sizeY, i, j));
+            printf("%02d ", getter(data, sizeY, i, j));
         printf("\n");
     }
 }
 
 
-void print_united (int data[])
+void print_united (const int data[])
 {
     print_separately(data+2, data[0], data[1]);
 }
 
+
+int getter(const int data[], size_t row_size, size_t posX, size_t posY)
+{
+    return *(data + row_size*posX + posY); 
+}
+
+int setter(int data[], size_t row_size, size_t posX, size_t posY, int new_val)
+{
+    int old_val = *(data + row_size*posX + posY);
+    *(data + row_size*posX + posY) = new_val;
+    return old_val;
+}
 
 int get_set(char mode, int data[], size_t row_size, size_t posX, size_t posY, int new_val)
 {
@@ -26,8 +38,8 @@ int get_set(char mode, int data[], size_t row_size, size_t posX, size_t posY, in
     
     else if(mode == 's')
     {
-        int old_val = data[row_size*posX + posY];
-        data[row_size*posX + posY] = new_val;
+        int old_val = *(data + row_size*posX + posY);
+        *(data + row_size*posX + posY) = new_val;
         return old_val;
     }
     else
--- lib.hpp
+++ lib.hpp
@@ -10,6 +10,8 @@ enum ERROR_CODES {
     GET_SET_ERROR   = 3  
 };
 
-void  print_separately  ( int data[], size_t sizeX, size_t sizeY );
-void  print_united      ( int data[] );
+void  print_separately  (const int data[], size_t sizeX, size_t sizeY );
+void  print_united      (const int data[] );
+int   getter            (const int data[], size_t row_size, size_t posX, size_t posY);
+int   setter            (int data[], size_t row_size, size_t posX, size_t posY, int new_val);
 int   get_set           ( char mode, int *data, size_t row_size, size_t posX, size_t posY, int new_val = 0 );
