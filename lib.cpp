c41f1d390658fa781b9aa0625e9a86f7a12ebcbd
Kirill Madorskii <madorskii.kv@phystech.edu>
2023-09-15 20:32:46 +0300

    getline added + strdup

--- lib.cpp
+++ lib.cpp
@@ -24,7 +24,7 @@ int read_text(char* buf, size_t buf_len, Text_info* Text, FILE* input)
 void print_text(Text_info *Text)
 {    
     for (size_t i = 0; i < Text->row_num; i++)
-        printf("%03d - %p - %s\n", i, &(Text->text[i]), Text->text[i]);
+        printf("%03ld - %p| %s", i, &(Text->text[i]), Text->text[i]);
 }
 
 
@@ -33,20 +33,4 @@ void free_text(Text_info *Text)
     for (size_t i = 0; i < Text->row_num; i++)
         free(Text->text[i]);
     free(Text->text);
-}
-
-
-int strfill(char* str1, char* str2)
-{
-    size_t i = 0;
-
-    while(str2[i] != '\n')
-    {
-        assert( i >= strlen(str1) );
-        str1[i] = str2[i];
-        i++;
-    }
-    str1[i] = '\0';
-    
-    return i;
 }
